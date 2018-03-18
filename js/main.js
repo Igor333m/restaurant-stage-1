4ffb947909a1cba866c526c117ff9a4d1d513b71
Igor333m <igor333m@yahoo.com>
2018-03-18 14:00:36 -0700

    feat: Add picture titles

--- js/main.js
+++ js/main.js
@@ -141,6 +141,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.title = DBHelper.titleForRestaurant(restaurant);
   li.append(image);
 
   const name = document.createElement('h2');
